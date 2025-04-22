c44c2e244c4750075b4f22dcc69a9286bb1df6b2
Contzo <ilierazvan1497@outlook.com>
2025-04-22 14:23:09 +0300

    Implemented the VRF request

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -26,6 +26,8 @@
  */
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title Raffle contract
@@ -33,13 +35,20 @@ pragma solidity 0.8.19;
  * @notice This contract is creating a simple raffle
  * @dev Implements Chianlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /** State variables */
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
+    /**Constants */
+    uint32 private constant NUMWORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
     //**Immutable and constatns */
     uint256 private immutable i_entranceFee;
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     // @dev The dudration of the lottery in seconds
     uint256 private immutable i_interval;
 
@@ -49,10 +58,20 @@ contract Raffle {
     /**Errors */
     error Raffle__SendMoreToEnterRaffle();
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCooridnator,
+        bytes32 _keyHash,
+        uint64 _subscriptionId,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCooridnator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp; // set the last time stamp to the current block timestamp
+        i_keyHash = _keyHash;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -68,10 +87,27 @@ contract Raffle {
         if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert();
         }
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUMWORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
     /**Getter Functions */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {}
 }
