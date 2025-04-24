038a294523d27301a59429d6acb47ea02c4d0799
Contzo <ilierazvan1497@outloook.com>
2025-04-24 20:15:38 +0300

    modules

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -26,7 +26,7 @@
  */
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.19;
-import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
