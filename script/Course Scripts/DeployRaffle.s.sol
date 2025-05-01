a7a034abae8dbc936fa492e71df2337916ceb3e5
Contzo <ilierazvan1497@outloook.com>
2025-05-01 15:18:33 +0300

    Finished deploy raffle

---
+++ script/Course Scripts/DeployRaffle.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployRaffle is Script {
+    function run() external {}
+
+    function deployContract()
+        public
+        returns (Raffle raffle, HelperConfig activeNetworkConfig)
+    {
+        HelperConfig helperConfig = new HelperConfig();
+        // for local network deploy a mock and get local config
+        // sepolia gets the sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig
+            .getNetworkConfig();
+        vm.startBroadcast();
+        raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.keyHash,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
+}
